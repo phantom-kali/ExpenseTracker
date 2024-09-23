52afc8203042933f15f6dcf9153cd031fd4f6361
Benard Karanja <benardopeter4@gmail.com>
2024-09-23 12:48:14 +0300

    Remove logout.html and redirect users to login.html after logout

--- users/urls.py
+++ users/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('login/', auth_views.LogoutView.as_view(template_name='users/login.html'), name='logout'),
 ]
