225aed90d2d6802d10875088b9b2e8202c8cddb5
Benard Karanja <benardopeter4@gmail.com>
2024-09-23 19:27:46 +0300

    Modified all the date input to be visible and styled them

--- expenses/forms.py
+++ expenses/forms.py
@@ -8,19 +8,24 @@ class CategoryForm(forms.ModelForm):
         model = Category
         fields = ['name']
 
+from django import forms
+from .models import Budget
+
 class BudgetForm(forms.ModelForm):
     class Meta:
         model = Budget
         fields = ['amount', 'start_date', 'end_date']
         widgets = {
-            'start_date': forms.DateInput(attrs={'type': 'date'}),
-            'end_date': forms.DateInput(attrs={'type': 'date'}),
+            'amount': forms.TextInput(attrs={'class': 'date-input-container'}),
+            'start_date': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+            'end_date': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
         }
+
 
 class ExpenseForm(forms.ModelForm):
     class Meta:
         model = Expense
         fields = ['amount', 'date', 'description', 'category']
         widgets = {
-            'date': forms.DateInput(attrs={'type': 'date'}),
+            'date': forms.DateInput(attrs={'type': 'date', 'class': 'form-control date-input-container'}),
         }
