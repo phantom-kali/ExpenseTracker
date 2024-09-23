5bf3f460657b13dd3bf6db7952e95f3e92a478f6
fideleliud <fideleliudclimax@gmail.com>
2024-09-23 12:55:03 +0300

    logout redirect to the login page

--- users/urls.py
+++ users/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('login/', auth_views.LogoutView.as_view(template_name='users/login.html'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),  # Redirect to login after logout
 ]
