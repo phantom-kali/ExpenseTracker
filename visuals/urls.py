0866390946e1f285ec3f50d99ba49c70a2c86a2a
Benard Karanja <benardopeter4@gmail.com>
2024-11-07 19:39:53 +0300

    removed expenses_over_time_view

--- visuals/urls.py
+++ visuals/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('statistics/', views.statistics_view, name='statistics'),
     path('budget-vs-expenses/', views.budget_vs_expenses_view, name='budget_vs_expenses'),
     path('expenses-by-category/', views.expenses_by_category_view, name='expenses_by_category'),
-    path('expenses-over-time/', views.expenses_over_time_view, name='expenses_over_time'), 
+     
 ]
